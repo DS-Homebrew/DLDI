7c1c49adfaf4861ed1be3367963cd900e19e5f76
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2023-04-27 13:48:34 -0700

    ak2_sd: thumb, LTO, -Oz
    2808 bytes -> 1996 bytes

--- source/ak2_sd/source/rpgprotocol.cpp
+++ source/ak2_sd/source/rpgprotocol.cpp
@@ -28,12 +28,7 @@
 #define PAGESIZE_2K     (3<<24)
 #define PAGESIZE_4K     (4<<24)
 
-void __attribute__((naked)) ioRpgDelay(u32 us)
-{
-    __asm("subs r0, #1");
-    __asm("bgt ioRpgDelay");
-    __asm("bx lr");
-}
+void __attribute__((naked)) ioRpgDelay(u32 us);
 
 void ioRpgSendCommand( u32 command[2], u32 pageSize, u32 latency, void * buffer )
 {

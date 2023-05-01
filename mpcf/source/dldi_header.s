e95d26b1059aa7e4de6a3dcfeeb1a800845bdafe
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2023-04-30 23:28:10 -0700

    mpcf: rebase to libnds 1.8.2

--- mpcf/source/dldi_header.s
+++ mpcf/source/dldi_header.s
@@ -1,36 +1,26 @@
+#include <nds/arm9/dldi_asm.h>
+
 @---------------------------------------------------------------------------------
-	.section ".init"
+	.section ".crt0","ax"
 @---------------------------------------------------------------------------------
 	.global _start
 	.align	4
 	.arm
 
 @---------------------------------------------------------------------------------
-.equ FEATURE_MEDIUM_CANREAD,		0x00000001
-.equ FEATURE_MEDIUM_CANWRITE,		0x00000002
-.equ FEATURE_SLOT_GBA,				0x00000010
-.equ FEATURE_SLOT_NDS,				0x00000020
-
-.equ FIX_ALL,						0x01
-.equ FIX_GLUE,						0x02
-.equ FIX_GOT,						0x04
-.equ FIX_BSS,						0x08
-
-
-@---------------------------------------------------------------------------------
 @ Driver patch file standard header -- 16 bytes
 	.word	0xBF8DA5ED		@ Magic number to identify this region
 	.asciz	" Chishm"		@ Identifying Magic string (8 bytes with null terminator)
 	.byte	0x01			@ Version number
-	.byte	0x0B	@2KiB	@ Log [base-2] of the size of this driver in bytes.
+	.byte	DLDI_SIZE_16KB
 	.byte	FIX_GOT | FIX_BSS	@ Sections to fix
-	.byte 	0x00			@ padding
-	
+	.byte 	0x00			@ Space allocated in the application, not important here.
+
 @---------------------------------------------------------------------------------
 @ Text identifier - can be anything up to 47 chars + terminating null -- 48 bytes
 	.align	4
 	.asciz "GBA Movie Player (Compact Flash)"
-	
+
 @---------------------------------------------------------------------------------
 @ Offsets to important sections within the data	-- 32 bytes
 	.align	6
@@ -45,7 +35,7 @@
 
 @---------------------------------------------------------------------------------
 @ IO_INTERFACE data -- 32 bytes
-	.ascii	"MPCF"
+	.ascii	"MPCF"			@ ioType
 	.word	FEATURE_MEDIUM_CANREAD | FEATURE_MEDIUM_CANWRITE | FEATURE_SLOT_GBA
 	.word	startup			@ 
 	.word	isInserted		@ 
@@ -53,7 +43,7 @@
 	.word	writeSectors	@ 
 	.word	clearStatus		@ 
 	.word	shutdown		@ 
-	
+
 @---------------------------------------------------------------------------------
 _start:
 @---------------------------------------------------------------------------------

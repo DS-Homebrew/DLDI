82b6215b7b6f8b9ed4dec9233995853f108f4ae5
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2023-06-09 18:00:28 -0700

    scds3: re-define commands using SDIO definitions
    This is definitely SDIO. We should make that clear.

--- source/scds3/source/scds.h
+++ source/scds3/source/scds.h
@@ -1,11 +1,11 @@
 /*
-	SuperCard DSONE
-	Card IO routines
+    SuperCard DSONE
+    Card IO routines
 
-	Copyright (C) 2006 SuperCard
-	Copyright (C) 2023 lifehackerhansol
+    Copyright (C) 2006 SuperCard
+    Copyright (C) 2023 lifehackerhansol
 
-	SPDX-License-Identifier: Zlib
+    SPDX-License-Identifier: Zlib
 */
 
 #pragma once
@@ -13,16 +13,16 @@
 #include <nds/ndstypes.h>
 
 #ifndef NULL
- #define NULL 0
+    #define NULL 0
 #endif
 
 // SCDS defines
-// SCDS ROMCTRL flags
+// SCDS MCCNT1 flags
 #define SCDS_CTRL_BASE      MCCNT1_ENABLE | MCCNT1_RESET_OFF | MCCNT1_LATENCY2(24) | MCCNT1_LATENCY1(0)
 #define SCDS_CTRL_READ_4B   SCDS_CTRL_BASE | MCCNT1_LEN_4
 #define SCDS_CTRL_READ_512B SCDS_CTRL_BASE | MCCNT1_LEN_512
 
-// SCDS CARD_COMMANDs
+// SCDS MCCMDs
 
 // return 0 == idle
 // return non-0 == not idle
@@ -34,33 +34,46 @@
 #define SCDS_CMD_SD_WRITE_END    (0x35ull << 56)
 #define SCDS_CMD_SD_READ_DATA    (0x36ull << 56)
 
-// 0x33 command has a whole lot of parameters. Will be split as needed
-#define SCDS_CMD_CARD_PARAM                     (0x33ull << 56) | (0x40ull << 40)
-#define SCDS_CMD_CARD_PARAM_R1                  SCDS_CMD_CARD_PARAM | 1ull
+// SDIO
+// 33 aa bb bb bb bb xx cc
+// aa = SDIO CMD & 0x40
+// bbbbbbbb = argument
+// cc = response type
+// xx = unused
 
-#define SCDS_CMD_SD_READ_MULTI_SECTOR_END      SCDS_CMD_CARD_PARAM | (12ull << 40)
+#define SCDS_CMD_SDIO_BASE       (0x33ull << 56) | (0x40ull << 40)
 
-static inline u64 SCDS_CMD_SD_READ_SINGLE_SECTOR_SEND(u32 sector)
+static inline u64 SCDS_CMD_SDIO(u8 response, u8 cmd, u32 argument)
 {
-	return SCDS_CMD_CARD_PARAM_R1 | (17ull << 40) | ((u64)sector << 8);
+    return SCDS_CMD_SDIO_BASE | ((u64)(cmd | 0x40) << 40) | ((u64)argument << 8) | (u64)response;
 }
 
-static inline u64 SCDS_CMD_SD_READ_MULTI_SECTOR_SEND(u32 sector)
+static inline u64 SCDS_CMD_SDIO_STOP_TRANSMISSION()
 {
-	return SCDS_CMD_CARD_PARAM_R1 | (18ull << 40) | ((u64)sector << 8);
+    return SCDS_CMD_SDIO(0, 12, 0);
+}
+
+static inline u64 SCDS_CMD_SDIO_READ_SINGLE_BLOCK(u32 sector)
+{
+    return SCDS_CMD_SDIO(1, 17, sector);
+}
+
+static inline u64 SCDS_CMD_SDIO_READ_MULTIPLE_BLOCK(u32 sector)
+{
+    return SCDS_CMD_SDIO(1, 18, sector);
 }
 
 // writing only works single sector
-static inline u64 SCDS_CMD_SD_WRITE_SECTOR_SEND(u32 sector)
+static inline u64 SCDS_CMD_SDIO_WRITE_SINGLE_BLOCK(u32 sector)
 {
-	return SCDS_CMD_CARD_PARAM_R1 | (24ull << 40) | ((u64)sector << 8);
+    return SCDS_CMD_SDIO(1, 24, sector);
 }
 
 // bytes == number of bytes currently sent
 // for each 4 bytes, write next word from buffer, up to 512 bytes
 static inline u64 SCDS_CMD_SD_WRITE_DATA(u32 bytes, u32 data)
 {
-	return (0x37ull << 56) | ((u64)bytes << 40) | ((u64)(__builtin_bswap32(data)) << 8);
+    return (0x37ull << 56) | ((u64)bytes << 40) | ((u64)(__builtin_bswap32(data)) << 8);
 }
 
 // user API

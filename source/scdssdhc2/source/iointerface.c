568535ff1af4cbbb091df2f064478778753be3b4
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-02-04 05:33:52 -0800

    Unify scdssdhc2/ttio, we are now scdssdhc2

--- source/scdssdhc2/source/iointerface.c
+++ source/scdssdhc2/source/iointerface.c
@@ -8,30 +8,36 @@
 #include "scdssdhc.h"
 #include "libtwl_card.h"
 
-static bool is_sdhc = false;
-
 // Initialize the driver. Returns true on success.
 bool startup(void)
 {
-	is_sdhc = SCDS_SendCommand(SCDS_CMD_SD_IS_SDHC()) != 0 ? true : false;
-    return true;
+#ifndef TTIO
+	SCDS_SDGetSDHCStatusFromSRAM();
+	return true;
+#else
+	return SCDS_SDInitialize();
+#endif
 }
 
 // Returns true if a card is present and initialized.
 bool is_inserted(void)
 {
-    return true;
+	return true;
 }
 
 // Reads 512 byte sectors into a buffer that may be unaligned. Returns true on
 // success.
 bool read_sectors(uint32_t sector, uint32_t num_sectors, void *buffer)
 {
+// Some devices based on TTIO breaks when trying to do both types of reads,
+// so stick with multi-sector reads.
+#ifndef TTIO
 	if (num_sectors == 1)
-    	SCDS_SDReadSingleSector(is_sdhc ? sector : sector << 9, buffer);
+		SCDS_SDReadSingleSector(sector, buffer);
 	else
-		SCDS_SDReadMultiSector(is_sdhc ? sector : sector << 9, buffer, num_sectors);
-    return true;
+#endif
+		SCDS_SDReadMultiSector(sector, buffer, num_sectors);
+	return true;
 }
 
 // Writes 512 byte sectors from a buffer that may be unaligned. Returns true on
@@ -39,20 +45,20 @@ bool read_sectors(uint32_t sector, uint32_t num_sectors, void *buffer)
 bool write_sectors(uint32_t sector, uint32_t num_sectors, const void *buffer)
 {
 	if (num_sectors == 1)
-    	SCDS_SDWriteSingleSector(is_sdhc ? sector : sector << 9, buffer);
+		SCDS_SDWriteSingleSector(sector, buffer);
 	else
-		SCDS_SDWriteMultiSector(is_sdhc ? sector : sector << 9, buffer, num_sectors);
-    return true;
+		SCDS_SDWriteMultiSector(sector, buffer, num_sectors);
+	return true;
 }
 
 // Clear error flags from the card. Returns true on success.
 bool clear_status(void)
 {
-    return true;
+	return true;
 }
 
 // Shutdowns the card. This may never be called.
 bool shutdown(void)
 {
-    return true;
+	return true;
 }
--- source/scdssdhc2/source/scdssdhc.h
+++ source/scdssdhc2/source/scdssdhc.h
@@ -24,6 +24,8 @@
 #define SCDS_CTRL_WRITE_4B         (SCDS_CTRL_BASE | MCCNT1_DIR_WRITE | MCCNT1_LEN_4)
 #define SCDS_CTRL_WRITE_512B       (SCDS_CTRL_BASE | MCCNT1_DIR_WRITE | MCCNT1_LEN_512)
 
+#define SCDS_CTRL_SD_LOW_CLK_LATENCY 0x1000
+
 // SCDS CARD_COMMANDs
 
 /*
@@ -95,7 +97,7 @@ enum SCDS_SD_HOST_MODES {
 	BB = SDIO command
 	CC = SD host mode, see SCDS_SD_HOST_MODES enum
 */
-static inline u64 SCDS_CMD_SD_HOST_PARAM(u32 parameter, u8 sdio, u8 mode)
+static inline u64 SCDS_CMD_SD_HOST_PARAM(u8 sdio, u32 parameter, u8 mode)
 {
 	return (0x51ull << 56) | ((u64)parameter << 24) | ((u64)sdio << 16) | ((u64)mode << 8);
 }
@@ -124,8 +126,32 @@ static inline u64 SCDS_CMD_SD_READ_MULTI_BLOCK(u32 sector)
 	return (0x54ull << 56) | ((u64)sector << 24);
 }
 
+/*
+	SD host control registers
+	The 0x5F sets raw registers related to the SD host, which is a single u8.
+	Bits:
+	0: Reset
+	1: Set 400k low clk
+	2: Use 0xB7 as alternative of 0x5B for ROM reads
+	3: Set SDHC mode
+	4-5: 1
+	6-7: 0
+*/
+#define SCDS_SD_HOST_REG_RESET BIT(0)
+#define SCDS_SD_HOST_REG_400KHZ_CLK BIT(1)
+#define SCDS_SD_HOST_REG_CLEAN_ROM_MODE BIT(2)
+#define SCDS_SD_HOST_REG_SDHC BIT(3)
+
+// This function will always set bits 4-5 to 1 and bits 6-7 to 0 for convenience
+static inline u64 SCDS_CMD_SD_HOST_SET_REGISTER(u8 bits)
+{
+	return (0x5F30ull << 48) | ((u64)bits << 48);
+}
+
 // user API
-u32 SCDS_SendCommand(const u64 command);
+u32 SCDS_SendCommand(const u64 command, u32 latency);
+void SCDS_SDGetSDHCStatusFromSRAM(void);
+bool SCDS_SDInitialize(void);
 void SCDS_SDReadSingleSector(u32 sector, void *buffer);
 void SCDS_SDReadMultiSector(u32 sector, void *buffer, u32 num_sectors);
 void SCDS_SDWriteSingleSector(u32 sector, const void *buffer);

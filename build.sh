9ae508f7cf6c01cbc62ea102c25f79a23a8ccf63
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-03-12 10:17:50 -0700

    {ak2,rpg,r4idsn}_sd: archive it
    These will be replaced with a rewritten, unified driver.

--- build.sh
+++ build.sh
@@ -22,14 +22,6 @@ for FILE in *; do
         make -C $FILE clean
         make -C $FILE SDHC=1
         cp $FILE/*.dldi $OUT
-    elif [[ $FILE == "ak2_sd" ]]; then
-        make -C $FILE clean
-        make -C $FILE -f Makefile_singlewrite clean
-        make -C $FILE
-        cp $FILE/*.dldi $OUT
-        make -C $FILE clean
-        make -C $FILE -f Makefile_singlewrite
-        cp $FILE/*.dldi $OUT
     else
         make -C $FILE clean
         make -C $FILE

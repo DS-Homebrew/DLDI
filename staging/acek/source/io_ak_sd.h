de93d8cf6feb144d9d356d372c08497b2a5eaf29
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-10-06 00:55:32 -0700

    acek: use pragma once

--- staging/acek/source/io_ak_sd.h
+++ staging/acek/source/io_ak_sd.h
@@ -3,13 +3,10 @@
     SPDX-License-Identifier: MIT
 */
 
-#ifndef _DIRECT_SD_H_
-#define _DIRECT_SD_H_
+#pragma once
 
 void dsd_send_sd_cmd( u8 cmd, u32 arg );
 
 void dsd_write_sd_sector( u32 address, const u8 * data );
 
 bool dsd_is_sd_busy();
-
-#endif//_DIRECT_SD_H_

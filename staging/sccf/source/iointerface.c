d59ed485956e7e70a36145f1831ad86fd506b574
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-11-02 13:23:37 -0700

    sccf: account for IDE sector limit

--- staging/sccf/source/iointerface.c
+++ staging/sccf/source/iointerface.c
@@ -152,17 +152,8 @@ bool clearStatus (void) {
 	return true;
 }
 
-
-/*-----------------------------------------------------------------
-readSectors
-Read 512 byte sector numbered "sector" into "buffer"
-u32 sector IN: address of first 512 byte sector on CF card to read
-u32 numSectors IN: number of 512 byte sectors to read,
- 1 to 256 sectors can be read
-void* buffer OUT: pointer to 512 byte buffer to store data in
-bool return OUT: true if successful
------------------------------------------------------------------*/
-bool readSectors (u32 sector, u32 numSectors, void* buffer) {
+bool readCardData(u32 sector, u32 numSectors, void* buffer)
+{
 	int i;
 
 	u16 *buff = (u16*)buffer;
@@ -225,18 +216,30 @@ bool readSectors (u32 sector, u32 numSectors, void* buffer) {
 	return true;
 }
 
-
-
 /*-----------------------------------------------------------------
-writeSectors
-Write 512 byte sector numbered "sector" from "buffer"
-u32 sector IN: address of 512 byte sector on CF card to read
+readSectors
+Read 512 byte sector numbered "sector" into "buffer"
+u32 sector IN: address of first 512 byte sector on CF card to read
 u32 numSectors IN: number of 512 byte sectors to read,
  1 to 256 sectors can be read
-void* buffer IN: pointer to 512 byte buffer to read data from
+void* buffer OUT: pointer to 512 byte buffer to store data in
 bool return OUT: true if successful
 -----------------------------------------------------------------*/
-bool writeSectors (u32 sector, u32 numSectors, void* buffer) {
+bool readSectors (u32 sector, u32 numSectors, void* buffer) {
+	while(numSectors > 0)
+	{
+		u32 sector_count;
+		sector_count = numSectors >= 256 ? 256 : numSectors;
+		if (!readCardData(sector, sector_count, buffer))
+			return false;
+		sector += sector_count;
+		numSectors -= sector_count;
+		buffer = (u8*)buffer + (0x200 * sector_count);
+	}
+	return true;
+}
+
+bool writeCardData(u32 sector, u32 numSectors, void* buffer) {
 	int i;
 
 	u16 *buff = (u16*)buffer;
@@ -297,6 +300,29 @@ bool writeSectors (u32 sector, u32 numSectors, void* buffer) {
 		}
 	}
 	
+	return true;
+}
+
+/*-----------------------------------------------------------------
+writeSectors
+Write 512 byte sector numbered "sector" from "buffer"
+u32 sector IN: address of 512 byte sector on CF card to read
+u32 numSectors IN: number of 512 byte sectors to read,
+ 1 to 256 sectors can be read
+void* buffer IN: pointer to 512 byte buffer to read data from
+bool return OUT: true if successful
+-----------------------------------------------------------------*/
+bool writeSectors (u32 sector, u32 numSectors, void* buffer) {
+	while(numSectors > 0)
+	{
+		u32 sector_count;
+		sector_count = numSectors >= 256 ? 256 : numSectors;
+		if (!writeCardData(sector, sector_count, buffer))
+			return false;
+		sector += sector_count;
+		numSectors -= sector_count;
+		buffer = (u8*)buffer + (0x200 * sector_count);
+	}
 	return true;
 }
 
